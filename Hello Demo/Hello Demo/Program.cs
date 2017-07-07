7427675ea7da5ed8ee4001025a3c6353c9f7a06a
Ravvone <ravvone.92@gmail.com>
2017-07-07 15:48:28 -0400

    Worked on hello world and user input

---
+++ Hello Demo/Hello Demo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Hello_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World");
+            //Console.ReadKey();
+
+            Console.WriteLine("We are here to become developers");
+            Console.WriteLine("today is Friday");
+            Console.WriteLine("We are here to become developers");
+            //Console.WriteLine("today is Friday");
+
+            Console.ReadKey();
+            WriteLine("When Console is declared you dont have to use that function");
+            WriteLine("now\n");
+            Write("nowagain");
+            ReadKey();
+
+
+
+        }
+    }
+}
---
+++ UserInput/UserInput/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            //string concatenation
+            Console.WriteLine("Hello " + name+ " how r u today");
+            Console.ReadLine();
+
+            Console.WriteLine("what is your age");
+            string age = Console.ReadLine();
+
+            Console.WriteLine("you are an {0} year old guy", age);
+
+            Console.WriteLine("{0} you are and {1} year old guy", name, age);
+            Console.ReadLine();
+
+            Console.WriteLine("are you going out tonight");
+            string goingout = Console.ReadLine();
+            //string interpalation
+            Console.WriteLine($"My name is {name} I am {age} and {goingout} of course");
+            Console.ReadLine();
+        }
+    }
+}
