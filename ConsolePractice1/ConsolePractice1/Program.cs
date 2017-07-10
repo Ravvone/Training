818d851ca48f85ac6a70912ed7d7055702a9bff0
Ravvone <ravvone.92@gmail.com>
2017-07-10 15:57:40 -0400

    practice

---
+++ ConsolePractice1/ConsolePractice1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsolePractice1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("What is your favorite name");
+            string dude = Console.ReadLine();
+            Console.WriteLine($"My name is {dude}");
+            Console.ReadKey();
+
+            Console.WriteLine("What is your age");
+            string age = Console.ReadLine();
+
+            Console.WriteLine("What's your Street name");
+            string streetName = Console.ReadLine();
+
+            Console.WriteLine("whats you email address");
+            string email = Console.ReadLine();
+
+            Console.WriteLine($"My age is {age}, my street name is {streetName}, my email address is {email} ");
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ ConsolePractice2/ConsolePractice2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsolePractice2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Whats your favorite amusement park");
+            string park = Console.ReadLine();
+
+            Console.WriteLine("Whens the last time you went there");
+            string when = Console.ReadLine();
+
+            Console.WriteLine("Did your have fun");
+            string did = Console.ReadLine();
+
+            Console.WriteLine($"My favorite amusment part is {park},the last time i went was {when}, {did} I had a good time ");
+            Console.ReadKey();
+        }
+    }
+}
