b60383ddd224048b7a5a1127d53c9fa9ce79a35a
Ravvone <ravvone.92@gmail.com>
2017-07-12 11:23:28 -0400

    else and if with intergers

---
+++ IfElsePracticeNumbers/IfElsePracticeNumbers/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IfElsePracticeNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give me a number bro");
+            string number1 = Console.ReadLine();
+            int num1 = Convert.ToInt32(number1);
+
+            Console.WriteLine("Give me another number bro");
+            string number2 = Console.ReadLine();
+            int num2 = Convert.ToInt32(number2);
+
+            if (num1 > num2)
+            {
+                Console.WriteLine($"first number is greater than your second number");
+            }
+            else if (num1 < num2)
+            {
+                Console.WriteLine($"{num1} is less than {num2}");
+            }
+            else
+            {
+                Console.WriteLine($"{num1} is equal to {num2}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
--- TernaryPractice1/TernaryPractice1/Program.cs
+++ TernaryPractice1/TernaryPractice1/Program.cs
@@ -10,6 +10,31 @@ namespace TernaryPractice1
     {
         static void Main(string[] args)
         {
+            //Console.WriteLine("what is your favorite soprot");
+            //string sport = Console.ReadLine().ToLower();
+            //string favSport = (sport == "hockey" || sport == "football" || sport == "golf") ? "great choice" : "bad choice";
+            //Console.WriteLine(favSport);
+            //Console.ReadLine();
+
+            //Console.WriteLine("what kind of foods do you like");
+            //string food = Console.ReadLine().ToLower();
+            //string food2 = Console.ReadLine().ToLower();
+            //string foodChoice = (food == "chicken") && (food2 == "shrimp") ? "good choice" : "bad choice";
+            //Console.WriteLine(foodChoice);
+            //Console.ReadLine();
+
+
+            Console.WriteLine("Who the best");
+            string best = Console.ReadLine().ToLower();
+            string best2 = Console.ReadLine().ToLower();
+
+
+            string thebest = (best == "we" && best2 == "the best") ? "that's damn right" : "you trippin";
+            Console.WriteLine(thebest);
+            Console.WriteLine($"You already know {best} {best2}");
+            Console.ReadLine();
+
+
         }
     }
 }
