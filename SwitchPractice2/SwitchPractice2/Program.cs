013812f5f5e4662de0f5a2d69f790efaf8052527
Ravvone <ravvone.92@gmail.com>
2017-07-13 12:30:08 -0400

    int

---
+++ SwitchPractice2/SwitchPractice2/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace SwitchPractice2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Who the best basketball player");
+            string best = Console.ReadLine().ToLower();
+
+            switch(best)
+            {
+                case "kobe":
+                    Console.WriteLine("possibly");
+                    break;
+                case "lebron":
+                    Console.WriteLine("possibly to !");
+                    break;
+                case "jordan":
+                    Console.WriteLine("Most would say");
+                    break;
+                default:
+                    Console.WriteLine("Well it must be eroc !!");
+                    break;
+
+
+            }
+            Console.ReadLine();
+
+            int num;
+            Console.WriteLine("what was jordans number for the bulls \n 1. 23 \n 2. 32 \n 3. 55");
+            string jordannum = Console.ReadLine();
+            int.TryParse(jordannum, out num);
+
+            switch(num)
+            {
+                case 1:
+                    Console.WriteLine("You are correct");
+                    break;
+                case 2:
+                    Console.WriteLine("Nope almost");
+                    break;
+                case 3:
+                    Console.WriteLine("Not even close brah");
+                    break;
+                default:
+                    Console.WriteLine("You got to be freaking kidding me");
+                    break;
+
+                
+            }
+            Console.ReadLine();
+           
+
+        }
+    }
+}
