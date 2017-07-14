ca02534ace1ae4db92119de69237426232692bf2
Ravvone <ravvone.92@gmail.com>
2017-07-14 13:34:09 -0400

    scope practice

---
+++ ScopePractice/ScopePractice/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScopePractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1 = 1;
+            int number2 = 2;
+
+            int sum = number1 + number2;
+
+            Console.WriteLine($"Then {number1} + {number2} equals = {sum}");
+            Console.ReadLine();
+
+        }
+
+        public static void Printing()
+        {
+            Console.WriteLine("This is great");
+            Console.ReadLine();
+        }
+
+        public static void Display()
+        {
+            Console.WriteLine("");
+        }
+
+        public static void Muliply()
+        {
+
+        }
+
+
+    }
+    public class Start
+    {
+
+    }
+}
