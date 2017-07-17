e2c726b7b2a03f9218094518f70f68bc18eb4813
Ravvone <ravvone.92@gmail.com>
2017-07-17 11:48:05 -0400

    Arrays with Looping

---
+++ LoopingPractice1/LoopingPractice1/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopingPractice1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int i = 0;
+            //i < 10;
+            //i++; 
+
+            int[] numbers = { 2, 4, 88, 11, 55, 99, 165 };
+            string[] stars = { "aeyonce", "ahianna", "aodonna", "another hoe" };
+
+            int[] dollars = { 20, 40, 60, 80, 100 };
+
+
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"The value of i is {i}");
+            }
+
+            //Console.ReadLine();
+
+            //for (int k = 0; k < stars.Length; ++k)
+            //{
+            //    Console.WriteLine("The stars are " + (stars[k]));
+            //}
+            //Console.ReadLine();
+
+            for (int g = 0; g < dollars.Length; ++g)
+            {
+                Console.WriteLine("When im trapping my money be flippin like " + (dollars[g]));
+
+            }
+            Console.ReadLine();
+
+            for (int d = 0; d < numbers.Length; ++d)
+            {
+                Array.Reverse(numbers);
+                Console.WriteLine(numbers[d]);
+            }
+            Console.ReadLine();
+
+            for (int e = 0; e < stars.Length; ++e)
+            {
+                Array.Reverse(stars);
+                Console.WriteLine(stars[e]);
+            }
+            Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ LoopingPractice2/LoopingPractice2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopingPractice2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] girls = { "india", "shinia", "quishonti", "shannon" };
+
+            //using the string 
+            for (int g = 0; g < girls.Length; ++g)
+            {
+                Console.WriteLine("my favorite girls are " + (girls[g]));
+
+            }
+            Console.ReadKey();
+
+            //using a array command
+            for (int d = 0; d < girls.Length; ++d)
+            {
+                Array.Sort(girls);
+                Console.WriteLine("my favorite girls are " + girls[d]);
+            }
+            Console.ReadKey();
+
+            //interoplation can only be used with number based system
+            for (int a = 0; a < girls.Length; ++a)
+            {
+                Console.WriteLine($"my favorite girls are {a}");
+            }
+            Console.ReadKey();
+
+
+
+
+        }    
+    }
+}
