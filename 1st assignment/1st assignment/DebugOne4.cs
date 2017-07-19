9db8b0b7b959cbf9774d683524889fd02fdb52bf
Ravvone <ravvone.92@gmail.com>
2017-07-19 15:59:58 -0400

    Assignment

--- 1st assignment/1st assignment/DebugOne4.cs
+++ 1st assignment/1st assignment/DebugOne4.cs
@@ -6,11 +6,11 @@ class DebugOne4
    static void Main()
    {
       WriteLine("Spinach dip:");
-      WriteLine();
+    
       WriteLine("1 package frozen chopped spinach");
       WriteLine("1 cup mayonaise");
       WriteLine("1 chopped onion");
-      WriteLine;
+     
       WriteLine("Mix ingredients together.");
       WriteLine("Chill and serve with chips.");
    }
