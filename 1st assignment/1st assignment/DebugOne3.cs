40787ad3a03e8316b20e3a6a0be97c2e07d02583
Ravvone <ravvone.92@gmail.com>
2017-07-19 14:00:10 -0400

    jdjdj

---
+++ 1st assignment/1st assignment/DebugOne3.cs
@@ -0,0 +1,14 @@
+// Filename DebugOne3.cs
+// This program outputs directions to a party
+class DebugOne3
+{
+    static void Main()
+    {
+        System.Console.WriteLine("Take Highway 51 north");
+        System.Console.WriteLine("Exit at County Road H");
+        System.Console.WriteLine("Go three blocks and make a right on School Street");
+        System.Console.WriteLine("Go right at Elm");
+        System.Console.WriteLine("Party is four houses down on the left");
+    }
+
+}
