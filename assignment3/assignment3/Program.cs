63ef72dfab07bd3fa3fa3fa42f3330ebb2562a1d
Ravvone <ravvone.92@gmail.com>
2017-07-27 10:33:37 -0400

    debug

--- assignment3/assignment3/Program.cs
+++ assignment3/assignment3/Program.cs
@@ -14,12 +14,17 @@ namespace assignment3
             DebugFive2 p = new DebugFive2();
             DebugFive3 m = new DebugFive3();
             DebugFive4 n = new DebugFive4();
+            DebugSix01 b = new DebugSix01();
+            DebugSix02 v = new DebugSix02();
 
             //f.fiveone();
             //p.fivetwo();
             //m.fivethree();
             //n.fivefour();
-                           
+            //b.sixone();
+            v.sixtwo();
+
+
         }
     }
 }
