4b4d616dba50b8ecc2c199c89d4457813aeba4c9
Ravvone <ravvone.92@gmail.com>
2017-08-07 19:02:32 -0400

    Prices Prices GOing UPP!!!

---
+++ C Sharp practice/C Sharp practice/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Breakfast special = new Breakfast("French toast", 4.99);
+            
+            //Display the info about breakfast
+            Console.WriteLine(special.INFO);
+            // then display today's special
+            Console.WriteLine("Today we are having {0} for {1}",special.Name, special.Price.ToString("C2"));
+            Console.ReadLine(); 
+
+
+        }
+       
+    }
+   
+    class Breakfast
+    {
+        public string INFO = "Breakfast is the most important meal of the day.";
+        private string name;
+        private double price;
+        public Breakfast(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+
+    }
+}
